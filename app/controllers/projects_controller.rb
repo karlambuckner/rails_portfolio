bfa8ec43b218b312ee35d14bf00aa44edf7e48d6
Karla Buckner <kmb2662@gmail.com>
2017-12-01 13:18:10 -0800

    add edit and delete for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,19 +4,46 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
   def new
     @project = Project.new
   end
 
   def create
-   @project = Project.new(project_params)
-   if @project.save
+    @project = Project.new(project_params)
+    if @project.save
      flash[:notice] = "Project Submitted"
      redirect_to projects_path
-   else
+    else
      render :new
-   end
- end
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+    @users = User.all
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @users = User.all
+    if @project.update(project_params)
+      flash[:notice] = "Project Updated!"
+      redirect_to projects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project successfully deleted!"
+    redirect_to projects_path
+  end
 
   private
   def project_params
