4f97ff34f20c9085422beac31d7b4d95d6159df6
Karla Buckner <kmb2662@gmail.com>
2017-12-01 20:01:21 -0800

    drop trying to do admin and only allow current user to add/edit/delete

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,5 @@
 class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index, :show]
-
-  before_action :only => [:new, :edit] do
-    redirect_to new_user_session_path unless current_user && current_user.admin
-  end
-
-
   def index
     @projects = Project.all
   end
