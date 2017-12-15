801e41849ceffc99796cd7854a2f889bd961a071
Karla Buckner <kmb2662@gmail.com>
2017-12-15 14:41:58 -0800

    attempt to get jQuery working in rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
+= require jquery-ujs
+= require turbolinks
+= require_tree .
+
+$(document).ready(function(){
+	$('#page_effect').fadeIn(5000);
+});
